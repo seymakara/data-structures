38d22239eb0181b8ca600da6cf5b748c36cc6f84
seymakara <seymakara88@gmail.com>
2018-11-07 13:29:01 -0800

    one test missing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var head = 0
+  var tail = 0
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[head] = value
+    head ++
   };
 
   someInstance.dequeue = function() {
+    if((head - tail) === 0 ){
+      return 0
+    }
+    var result = storage[tail]
+    delete storage[tail]
+    tail ++
+    console.log("res", result)
+    return result
   };
 
   someInstance.size = function() {
+    return head - tail
+
   };
 
   return someInstance;
