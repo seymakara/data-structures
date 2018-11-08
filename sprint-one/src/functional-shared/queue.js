319f4ee9b5f95858ef4e468abb1016bfd07bf9f8
seymakara <seymakara88@gmail.com>
2018-11-07 16:27:26 -0800

    complete and working

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,41 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var queue = {
+    storage: {},
+    head: 0,
+    tail: 0
+  };
+
+  extend(queue, queueMethods);
+  return queue;
+}
+
+var extend = function(to, from) {
+  for (var key in from) {
+      to[key] = from[key];
+  }
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.head] = value
+    this.head ++
+  },
+  size: function() {
+    return this.head - this.tail
+  },
+  dequeue: function() {
+    if(this.head - this.tail === 0 ){
+        return 0
+    }
+    var result = this.storage[this.tail]
+    // console.log("storage", this.storage[this.tail])
+    // console.log("res", result)
+    delete this.storage[this.tail]
+    this.tail ++
+    return result
+  }
+}
+
+
 
 
