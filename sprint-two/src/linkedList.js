ba6231fa948e217c7046e654d7479c3854099d26
seymakara <seymakara88@gmail.com>
2018-11-09 10:49:13 -0800

    complete linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,26 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-    var itemToAdd = Node();
-    itemToAdd.value = value
+    var itemToAdd = Node(value);
     
     if (list.head === null) {
-      list.head = itemToAdd
-    }else{
-      var current = list.head
-      while(current.next !== null){
-        current =current.next
-      }
-      current.next = itemToAdd
+      list.head = itemToAdd;
+      list.tail = itemToAdd;
+    } else {
+      list.tail.next = itemToAdd;
+      list.tail = itemToAdd;
     }
-    return list.head
     
   };
 
   list.removeHead = function() {
+    var removed = list.head;
+    list.head = list.head.next;
+    return removed.value;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
   };
 
   return list;
