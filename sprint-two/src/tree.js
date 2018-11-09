7376b9f42195dbf0fcbfe84c2e52aae54f669270
seymakara <seymakara88@gmail.com>
2018-11-09 11:28:12 -0800

    complete tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,39 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  
+  extend(newTree, treeMethods);
+  
   return newTree;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var childNode = Tree(value)
+  this.children.push(childNode)
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target){
+    return true
+  }else{
+    if (this.children){
+      for(var i = 0; i < this.children.length; i++){
+        if(this.children[i].contains(target) === true){
+          return true
+        }
+      }
+    }
+  }
+  return false
 };
 
 
