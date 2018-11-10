8279307f8fd396fa3ba4e994ded1b52beca2295c
seymakara <seymakara88@gmail.com>
2018-11-09 21:55:44 -0800

    complete binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,84 @@
+
 var BinarySearchTree = function(value) {
+  var newBinaryTree = {};
+  newBinaryTree.value = value;
+  
+  newBinaryTree.left = null;
+  newBinaryTree.right = null;
+  
+  extend(newBinaryTree, binaryTreeMethods);
+  
+  return newBinaryTree;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+var binaryTreeMethods = {};
+
+
+binaryTreeMethods.insert = function(value) {
+  var child = BinarySearchTree(value);
+
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = child;
+    } else {
+      this.left.insert(value);
+    }
+  }
+  
+  if (value > this.value) {
+    if (this.right === null) {
+      this.right = child;
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+binaryTreeMethods.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  };
+  
+  if (value < this.value) {
+    if (this.left === null) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+  };
+  
+  if (value > this.value) {
+    if (this.right === null) {
+      return false;
+    } else {
+      return this.right.contains(value); 
+    }
+  };
+};
+
+binaryTreeMethods.depthFirstLog = function(cb) {
+  cb (this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  };
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  };
+  
+  
 };
 
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+
+
+
+
+
+
+
