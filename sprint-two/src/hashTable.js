9e3dd5c71e61e6e487f6beee0852d52da88024a5
seymakara <seymakara88@gmail.com>
2018-11-10 15:47:12 -0800

    complete hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,43 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var indexBucket = this._storage.get(index);
+  
+  if (indexBucket === undefined) {
+    this._storage.set(index, [[k, v]]); 
+  } else { 
+    for (var i = 0; i < indexBucket.length; i++) {
+      if (indexBucket[i][0] === k) {
+        indexBucket[i][1] = v;
+      } else {
+        indexBucket.push([k, v]); 
+      }
+    }
+    this._storage.set(index, indexBucket);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var indexBucket = this._storage.get(index);
+  var result;
+  for (var i = 0; i < indexBucket.length; i ++) {
+    if (indexBucket[i][0] === k) {
+      result = indexBucket[i][1];
+    }
+  }
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var indexBucket = this._storage.get(index);
+  for (var i = 0; i < indexBucket.length; i ++) {
+    if (indexBucket[i][0] === k) {
+      return indexBucket.splice(i, 1);
+    }
+  }
+  
 };
 
 
