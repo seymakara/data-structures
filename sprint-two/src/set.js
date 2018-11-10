e92b91079e67ec9ee66a8e86106b70e23f383741
seymakara <seymakara88@gmail.com>
2018-11-09 18:52:43 -0800

    complete set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {};
+  // set.count = 0
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage[item] = item
 };
 
 setPrototype.contains = function(item) {
+  if (item in this.storage) {
+    return true
+  }
+  return false
 };
 
 setPrototype.remove = function(item) {
+  delete this.storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//edge case: keep track of num of items
