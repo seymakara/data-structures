e44deb9b50c1b0cb834c65281b3cb4d608fb2ceb
seymakara <seymakara88@gmail.com>
2018-11-25 21:21:40 -0800

    refactor the bare min reqs to ES6

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,29 +1,26 @@
-var Set = function() {
-  var set = Object.create(setPrototype);
-  set.storage = {};
-  // set.count = 0
-  return set;
-};
-
-var setPrototype = {};
-
-// O(1)
-setPrototype.add = function(item) {
-  this.storage[item] = item;
-};
-
-// O(1)
-setPrototype.contains = function(item) {
-  if (item in this.storage) {
-    return true;
+class Set{
+  constructor(){
+    this.storage = {};
   }
-  return false;
-};
 
-// O(1)
-setPrototype.remove = function(item) {
-  delete this.storage[item];
-};
+  // O(1)
+  add(item) {
+    this.storage[item] = item;
+  };
+
+  // O(1)
+  contains(item) {
+    if (item in this.storage) {
+      return true;
+    }
+    return false;
+  };
+
+  // O(1)
+  remove(item) {
+    delete this.storage[item];
+  };
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
