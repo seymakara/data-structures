1418315101ba582e2bc5cf3fc7634667a02cf5e0
seymakara <seymakara88@gmail.com>
2018-11-25 22:10:00 -0800

    refactor the bare min reqs to ES6

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,79 +1,67 @@
 
-var BinarySearchTree = function(value) {
-  var newBinaryTree = {};
-  newBinaryTree.value = value;
-  
-  newBinaryTree.left = null;
-  newBinaryTree.right = null;
-  
-  extend(newBinaryTree, binaryTreeMethods);
-  
-  return newBinaryTree;
-};
-
-var extend = function(to, from) {
-  for (var key in from) {
-    to[key] = from[key];
+class BinarySearchTree {
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-};
-
-var binaryTreeMethods = {};
 
 // O(log n)
-binaryTreeMethods.insert = function(value) {
-  var child = BinarySearchTree(value);
+  insert(value) {
+    var child = new BinarySearchTree(value);
 
-  if (value < this.value) {
-    if (this.left === null) {
-      this.left = child;
-    } else {
-      this.left.insert(value);
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = child;
+      } else {
+        this.left.insert(value);
+      }
     }
-  }
-  
-  if (value > this.value) {
-    if (this.right === null) {
-      this.right = child;
-    } else {
-      this.right.insert(value);
+    
+    if (value > this.value) {
+      if (this.right === null) {
+        this.right = child;
+      } else {
+        this.right.insert(value);
+      }
     }
-  }
-};
+  };
 
-//O(log n)
-binaryTreeMethods.contains = function(value) {
-  if (this.value === value) {
-    return true;
-  }
-  
-  if (value < this.value) {
-    if (this.left === null) {
-      return false;
-    } else {
-      return this.left.contains(value);
+  //O(log n)
+  contains(value) {
+    if (this.value === value) {
+      return true;
     }
-  }
-  
-  if (value > this.value) {
-    if (this.right === null) {
-      return false;
-    } else {
-      return this.right.contains(value); 
+    
+    if (value < this.value) {
+      if (this.left === null) {
+        return false;
+      } else {
+        return this.left.contains(value);
+      }
     }
-  }
-};
+    
+    if (value > this.value) {
+      if (this.right === null) {
+        return false;
+      } else {
+        return this.right.contains(value); 
+      }
+    }
+  };
 
-//O(n)
-binaryTreeMethods.depthFirstLog = function(cb) {
-  cb (this.value);
-  if (this.left) {
-    this.left.depthFirstLog(cb);
-  }
-  if (this.right) {
-    this.right.depthFirstLog(cb);
-  }
-  
-};
+  //O(n)
+  depthFirstLog(cb) {
+    cb (this.value);
+    if (this.left) {
+      this.left.depthFirstLog(cb);
+    }
+    if (this.right) {
+      this.right.depthFirstLog(cb);
+    }
+    
+  };
+}
 
 
 
