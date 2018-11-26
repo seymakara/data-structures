e72aed2d522e36b9ea1caa901e3a5c28a57af4b9
seymakara <seymakara88@gmail.com>
2018-11-25 21:49:05 -0800

    refactor the bare min reqs to ES6

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,53 +1,52 @@
+class HashTable {
+  constructor(){
+    this._limit = 8;
+    this._storage = LimitedArray(this._limit);
+  }
 
+  // O(n) but probably O(1)
+  insert(k, v) {
+    var index = getIndexBelowMaxForKey(k, this._limit);
+    var indexBucket = this._storage.get(index);
+    
+    if (indexBucket === undefined) {
+      this._storage.set(index, [[k, v]]); 
+    } else { 
+      for (var i = 0; i < indexBucket.length; i++) {
+        if (indexBucket[i][0] === k) {
+          indexBucket[i][1] = v;
+        } else {
+          indexBucket.push([k, v]); 
+        }
+      }
+      this._storage.set(index, indexBucket);
+    }
+  };
 
-var HashTable = function() {
-  this._limit = 8;
-  this._storage = LimitedArray(this._limit);
-};
-
-// O(n) but probably O(1)
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var indexBucket = this._storage.get(index);
-  
-  if (indexBucket === undefined) {
-    this._storage.set(index, [[k, v]]); 
-  } else { 
-    for (var i = 0; i < indexBucket.length; i++) {
+  //O(n) but probably O(1)
+  retrieve(k) {
+    var index = getIndexBelowMaxForKey(k, this._limit);
+    var indexBucket = this._storage.get(index);
+    var result;
+    for (var i = 0; i < indexBucket.length; i ++) {
       if (indexBucket[i][0] === k) {
-        indexBucket[i][1] = v;
-      } else {
-        indexBucket.push([k, v]); 
+        result = indexBucket[i][1];
       }
     }
-    this._storage.set(index, indexBucket);
-  }
-};
+    return result;
+  };
 
-//O(n) but probably O(1)
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var indexBucket = this._storage.get(index);
-  var result;
-  for (var i = 0; i < indexBucket.length; i ++) {
-    if (indexBucket[i][0] === k) {
-      result = indexBucket[i][1];
+  //O(n) but probably O(1)
+  remove(k) {
+    var index = getIndexBelowMaxForKey(k, this._limit);
+    var indexBucket = this._storage.get(index);
+    for (var i = 0; i < indexBucket.length; i ++) {
+      if (indexBucket[i][0] === k) {
+        return indexBucket.splice(i, 1);
+      }
     }
-  }
-  return result;
-};
-
-//O(n) but probably O(1)
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var indexBucket = this._storage.get(index);
-  for (var i = 0; i < indexBucket.length; i ++) {
-    if (indexBucket[i][0] === k) {
-      return indexBucket.splice(i, 1);
-    }
-  }
-  
-};
+  };
+}
 
 
 
